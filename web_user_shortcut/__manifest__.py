e20e37d91b84f4da38c8f33e5a13f3d419c7c96c
CuuNV <62234877+CuuNV@users.noreply.github.com>
2020-03-16 14:35:36 +0700

    first commit

---
+++ web_user_shortcut/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'User Shortcut Menu',
+    'price': 0.0,
+    'category': 'Extra Tools',
+    'license': 'AGPL-3',
+    'currency': 'EUR',
+    'version': '12.0.1',
+    'author': 'CuuNV',
+    'maintainer': 'CuuNV <nguyenvancuu.vp@gmail.com>',
+    'website': 'https://ko-fi.com/cuunv277',
+    'description': """
+    Shortcut menu
+    =============
+    Allow user add list of frequently used menu items and display in a systray item
+    """,
+    'depends': [
+        'base',
+        'web'
+    ],
+    'demo': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    'qweb': [
+        'static/src/xml/templates.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
